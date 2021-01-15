6be1a3c2d3f39ea23cefbb1bb0b811e8bab08207
lmd-dev <lesmoulinsdudev1@gmail.com>
2021-01-15 19:05:17 +0100

    Add some fields to Playlist

--- playlist.js
+++ playlist.js
@@ -1,12 +1,23 @@
 const YTVideos = require('./videos');
-
+/**
+ * Represents a playlist
+ */
 class YTPlaylist
 {
+    /**
+     * Constructor
+     * @param {*} data Initialization data
+     * @param {*} channel Parent channel of the playlist
+     */
     constructor(data, channel = null)
     {
         this.kind = data?.kind ?? this.kind;
         this.id = data?.id ?? this.kind;
-        this.snippet = data?.snippet ?? this.snippet;
+        this.publishedAt = new Date(data?.snippet?.publishedAt) ?? null;
+        this.title = data?.snippet?.title ?? "";
+        this.description = data?.snippet?.description ?? "";
+        this.thumbnails = data?.snippet?.thumbnails ?? {};
+        this.tags = data?.snippet?.tags ?? [];
 
         this.channel = channel;
         this.videos = new YTVideos(channel, this);
