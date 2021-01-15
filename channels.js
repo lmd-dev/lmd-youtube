0c9489b6763863a3bf6ceb0b1cb4f9186a6aab2e
lmd-dev <lesmoulinsdudev1@gmail.com>
2021-01-15 19:02:52 +0100

    Add some comments to channels.js

--- channels.js
+++ channels.js
@@ -1,13 +1,23 @@
 const YTChannel = require("./channel");
 const YTManager = require('./manager');
 
+/**
+ * Channels manager
+ */
 class YTChannels extends YTManager
 {
+    /**
+     * Constructor
+     */
     constructor()
     {
         super();
     }
 
+    /**
+     * Returns hcannels matching with the query
+     * @param {*} query 
+     */
     async list(query)
     {
         const data = await super.list(query, "channels");
