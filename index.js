b379b09ea4a92c8f9f8d33ba22294200fd38a67c
lmd-dev <lesmoulinsdudev1@gmail.com>
2021-01-15 18:54:55 +0100

    Add some comments to index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const YTChannels = require("./channels");
 
+/**
+ * Entry point of the API
+ */
 class Youtube
 {
+    //Unic instance of the API
     static _instance = null;
+
+    //Used Google Youtube API Key
     static apiKey = "";
 
+    /**
+     * [Private] Constructor
+     * @param {string} apiKey API Key to use 
+     */
     constructor(apiKey)
     {
         this.channels = new YTChannels();
@@ -12,6 +22,10 @@ class Youtube
         Youtube.apiKey = apiKey;
     }
 
+    /**
+     * Retunrs the unic instance of the API entry point
+     * @param {string} apiKey API Key to use 
+     */
     static getInstance(apiKey = null)
     {
         if (Youtube._instance === null)
