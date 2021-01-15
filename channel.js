7ed6dd261a29c7347e5928e7374ab1bcbe0b0561
lmd-dev <lesmoulinsdudev1@gmail.com>
2021-01-15 19:01:56 +0100

    Add some fields to Channel

--- channel.js
+++ channel.js
@@ -1,12 +1,25 @@
 const YTPlaylists = require('./playlists');
 
+/**
+ * Represents a Youtube Channel
+ */
 class YTChannel
 {
+    /**
+     * Constructor
+     * @param {*} data Initialization data 
+     */
     constructor(data)
     {
         this.kind = data?.kind ?? this.kind;
         this.id = data?.id ?? this.kind;
-        this.snippet = data?.snippet ?? this.snippet;
+        this.title = data?.snippet?.title ?? "";
+        this.description = data?.snippet?.description ?? "";
+        this.customUrl = data?.snippet?.customUrl ?? "";
+        this.publishedAt = new Date(data?.snippet?.publishedAt) ?? null;
+        this.thumbnails = data?.snippet?.thumbnails ?? {};
+        this.defaultLanguage = data?.snippet?.defaultLanguage ?? "";
+        this.country = data?.snippet?.country ?? "";
 
         this.playlists = new YTPlaylists(this);
     }
